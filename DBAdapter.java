3b63e4a08b060bd1ac68b1d569b18846232c0a19
Panagiotis Kritikakos <panoskrt@gmail.com>
2012-12-17 16:23:26 +0200

    Update DBAdapter.java
    Handling DB object within the class without requiring creating another DB instance on the calling class.

--- DBAdapter.java
+++ DBAdapter.java
@@ -1,19 +1,23 @@
-############################################################################
-# Copyright (C) 2012  Panagiotis Kritikakos <panoskrt@gmail.com>           #
-#                                                                          #
-#    This program is free software: you can redistribute it and/or modify  #
-#    it under the terms of the GNU General Public License as published by  #
-#    the Free Software Foundation, either version 3 of the License, or     #
-#    (at your option) any later version.                                   #
-#                                                                          #
-#    This program is distributed in the hope that it will be useful,       #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
-#    GNU General Public License for more details.                          #
-#                                                                          #
-#    You should have received a copy of the GNU General Public License     #
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>. #
-############################################################################
+package com.panoskrt.dbadapter;
+
+/**
+ ########################################################################
+ # Copyright (C) 2012 Panagiotis Kritikakos <panoskrt@gmail.com> 		#
+ # 																		#
+ # This program is free software: you can redistribute it and/or modify #
+ # it under the terms of the GNU General Public License as published by #
+ # the Free Software Foundation, either version 3 of the License, or 	#
+ # (at your option) any later version. 									#
+ # 																		#
+ # This program is distributed in the hope that it will be useful, 		#
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of 		#
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 		#
+ # GNU General Public License for more details. 						#
+ # 																		#
+ # You should have received a copy of the GNU General Public License 	#
+ # along with this program. If not, see <http://www.gnu.org/licenses/>. #
+ ########################################################################
+ **/
 
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -27,14 +31,17 @@ import java.net.URLConnection;
 
 import org.apache.http.util.ByteArrayBuffer;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.os.Build;
 
 public class DBAdapter {
 
+	private SQLiteDatabase database = null;
 	private String dbPath;
 	private String dbName;
 	private Context context;
@@ -46,14 +53,17 @@ public class DBAdapter {
 	}
 
 	public void copyDB() {
-		SQLiteDatabase checkDB = null;
 		try {
 			try {
-				checkDB = SQLiteDatabase.openDatabase(dbPath + dbName, null, SQLiteDatabase.OPEN_READONLY);
+				database = SQLiteDatabase.openDatabase(dbPath + dbName, null,
+						SQLiteDatabase.OPEN_READONLY);
 			} catch (SQLiteException e) {
-				// Checking if required dirs exist as sometimes they do not and they need to be created.
+				// Checking if required dirs exist as sometimes they do not and
+				// they need to be created.
 				File dbDir = new File(dbPath);
-				if (!dbDir.exists()){ dbDir.mkdirs(); }
+				if (!dbDir.exists()) {
+					dbDir.mkdirs();
+				}
 				// Copying db from assets to db directory.
 				InputStream in = context.getAssets().open(dbName);
 				OutputStream out = new FileOutputStream(dbPath + dbName);
@@ -62,24 +72,26 @@ public class DBAdapter {
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
-		if (checkDB != null) {
-			checkDB.close();
+		if (database != null) {
+			database.close();
 		}
 	}
 
 	public SQLiteDatabase openDB() {
-		SQLiteDatabase db = SQLiteDatabase.openDatabase(dbPath + dbName, null, SQLiteDatabase.OPEN_READONLY);
-		return db;
+		database = SQLiteDatabase.openDatabase(dbPath + dbName, null,
+				SQLiteDatabase.OPEN_READONLY);
+		return database;
 	}
 
-	public Cursor runQuery(SQLiteDatabase database, String query) {
+	public Cursor runQuery(String query) {
 		Cursor c = database.rawQuery(query, null);
 		return c;
 	}
-	
-	public String getStringEntry(SQLiteDatabase database, String column, String table, String match){
+
+	public String getStringEntry(String column, String table, String match) {
 		String text = null;
-		Cursor c = database.rawQuery("SELECT " + column + " FROM " + table + " WHERE " + column + "=" + "'" + match + "'", null);
+		Cursor c = database.rawQuery("SELECT " + column + " FROM " + table
+				+ " WHERE " + column + "=" + "'" + match + "'", null);
 		if (c != null) {
 			if (c.moveToFirst()) {
 				text = c.getString(c.getColumnIndex(column));
@@ -87,10 +99,11 @@ public class DBAdapter {
 		}
 		return text;
 	}
-	
-	public int getIntEntry(SQLiteDatabase database, String column, String table, String match){
+
+	public int getIntEntry(String column, String table, String match) {
 		int number = 0;
-		Cursor c = database.rawQuery("SELECT " +  column + " FROM " + table + " WHERE " + column + "=" + match, null);
+		Cursor c = database.rawQuery("SELECT " + column + " FROM " + table
+				+ " WHERE " + column + "=" + match, null);
 		if (c != null) {
 			if (c.moveToFirst()) {
 				number = c.getInt(c.getColumnIndex(column));
@@ -99,38 +112,74 @@ public class DBAdapter {
 		return number;
 	}
 
-	public long tableCount(SQLiteDatabase database, String tableName) {
+	public long getLongEntry(String column, String table, String match) {
+		long number = 0;
+		Cursor c = database.rawQuery("SELECT " + column + " FROM " + table
+				+ " WHERE " + column + "=" + match, null);
+		if (c != null) {
+			if (c.moveToFirst()) {
+				number = c.getInt(c.getColumnIndex(column));
+			}
+		}
+		return number;
+	}
+
+	public double getDoubleEntry(String column, String table, String match) {
+		double number = 0;
+		Cursor c = database.rawQuery("SELECT " + column + " FROM " + table
+				+ " WHERE " + column + "=" + match, null);
+		if (c != null) {
+			if (c.moveToFirst()) {
+				number = c.getInt(c.getColumnIndex(column));
+			}
+		}
+		return number;
+	}
+
+	public void dropTable(String tableName) {
+		database.rawQuery("DROP TABLE " + tableName, null);
+	}
+
+	public long tableCount(String tableName) {
 		long count = DatabaseUtils.queryNumEntries(database, tableName);
 		return count;
 	}
 
-	public void closeDB(SQLiteDatabase database) {
-		database.close();
-	}
-	
-	public void deleteDB(){
+	// The .deleteDatabase method is supported on API 16 and higher.
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+	public void deleteDB() {
 		File oldDB = new File(dbPath + dbName);
+		SQLiteDatabase.releaseMemory();
 		SQLiteDatabase.deleteDatabase(oldDB);
 	}
 
+	public SQLiteDatabase getDB() {
+		return database;
+	}
+	
+	public void closeDB() {
+		database.close();
+	}
+
 	public void downloadDB(Context context, String dbName, String urlLink) {
-		try {		
+		try {
 			URL url = new URL(urlLink);
 			URLConnection ucon = url.openConnection();
 
 			InputStream is = ucon.getInputStream();
 			BufferedInputStream bis = new BufferedInputStream(is);
 			ByteArrayBuffer baf = new ByteArrayBuffer(50);
-			
+
 			int current = 0;
 			while ((current = bis.read()) != -1) {
 				baf.append((byte) current);
 			}
 
-			FileOutputStream fos = context.openFileOutput(dbName, Context.MODE_PRIVATE);
+			FileOutputStream fos = context.openFileOutput(dbName,
+					Context.MODE_PRIVATE);
 			fos.write(baf.toByteArray());
 			fos.close();
-			
+
 			File dbFile = new File(context.getFilesDir() + "/" + dbName);
 			InputStream in = new FileInputStream(dbFile);
 			OutputStream out = new FileOutputStream(dbPath + dbName);
@@ -141,17 +190,17 @@ public class DBAdapter {
 			ex.printStackTrace();
 		}
 	}
-	
-	private void bufCopy(InputStream in, OutputStream out){
+
+	private void bufCopy(InputStream in, OutputStream out) {
 		try {
-		byte[] buf = new byte[1024];
-		int len;
-		while ((len = in.read(buf)) > 0) {
-			out.write(buf, 0, len);
-		}
-		in.close();
-		out.close();
-		} catch (Exception ex){
+			byte[] buf = new byte[1024];
+			int len;
+			while ((len = in.read(buf)) > 0) {
+				out.write(buf, 0, len);
+			}
+			in.close();
+			out.close();
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
